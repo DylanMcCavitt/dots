4a154737e838cadab2b89818784184cb0b0d1052
Dylan McCavitt <dylanmccavitt2015@gmail.com>
2025-12-02 00:22:15 -0500

    changes#cassius2

--- modules/home/kitty.nix
+++ modules/home/kitty.nix
@@ -1,7 +1,25 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: {
-  xdg.configFile."kitty/nord-kitty.conf".source = ./kitty/nord-kitty.conf;
+  programs.kitty = {
+    enable = true;
+
+    theme = "Nord";
+
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      size = 12;
+    };
+
+    settings = {
+      hide_window_decorations = "yes";
+      tab_bar_style = "hidden";
+      tab_bar_min = 1;
+      copy_on_select = "yes";
+      confirm_os_window_close = "0";
+    };
+  };
 }
